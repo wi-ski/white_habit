a292660e3e736722bff53d99be92edf4a2d4666a
William Dembinski <wdembiz@gmail.com>
2015-11-10 08:14:45 -0800

    Working from home

--- app/modules/certSign.js
+++ app/modules/certSign.js
@@ -3,9 +3,66 @@ var publicKey = require("./publicKey.js");
 var crypto = require('./jsencrypt.js');
 var shaGen = require("./sha256.js");
 
-
-
+zz=new crypto();
 qq = new crypto();
+// var publicKey=qq.getPublicKey();
+// var privateKey = qq.getPrivateKey();
+
+
+var test="FooFoo";
+
+
+qq.setPublicKey(publicKey);
+
+var enqMessage = qq.encrypt(test);
+console.log("qqqqqqqq",enqMessage)
+
+
+
+zz.setPrivateKey(privateKey)
+var answer = zz.decrypt(enqMessage)
+console.log("HOPE SHE BE FOO: ",answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// zz=new crypto();
+// qq = new crypto();
+// // var publicKey=qq.getPublicKey();
+// // var privateKey = qq.getPrivateKey();
+
+
+// var test="FooFoo";
+
+
+// qq.setPublicKey(publicKey);
+
+// var enqMessage = qq.encrypt(test);
+// console.log("qqqqqqqq",enqMessage)
+
+
+
+// zz.setPrivateKey(privateKey)
+// var answer = zz.decrypt(enqMessage)
+// console.log("HOPE SHE BE FOO: ",answer)
+
+
 module.exports.sign = function(UsersPublicKey){
   var key = new NodeRSA(privateKey,'private');
   var hashedKey = shaGen.hash(UsersPublicKey);
--- app/modules/publicKey.js
+++ app/modules/publicKey.js
@@ -1,1 +1,16 @@
-module.exports="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD0ccAlQv23adSHM41XphKmyy4XR3UdYMRV1te6nRRikUN2CAdmDnfKvhGKlLp20lqwVOX29c1sYCVR77U1Spgxn9IoINKdN3DrHjButFYETkcf+OobaaG1kCxXmvbt9VM6b4mCkCYxsj4cRozQ+//GOgiM5HWmF/v21NyGuGMy4wIDAQAB"
+
+var start = ['-----BEGIN PUBLIC KEY-----',
+'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDNvBxblJJaWFBGdZoalaALp7jA',
+'yKOijl0xBAeTHnK7ng9ZEjDMUC7nAEi9HX535wlGaOtkRLrSXmNLv7qFu3CpFmlk',
+'x02UcwcX+52nf6taS52bvgOnVmts6ShvPlmLW/2mJ+ormM9nZIXG3l2u/4dWxvdH',
+'1r2sc9Q/dxKvmtdpdQIDAQAB',
+'-----END PUBLIC KEY-----']
+
+// var start = ['-----BEGIN PUBLIC KEY-----',
+// 'MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGqRqwA3NScuw8lWn2angt/ZHJdd',
+// 'JeFtu1uCUyw/yDcB1oaCRkUpf3tqVQt3fXUvK4pVg0MndLGOkbiTGyIrHVWNyKY9',
+// 't38DxtdPBig95TQqbmM+q6D2xiwPI8KT4l6t3dywF/oq1C6cHwIPRc5yUZS/nH0J',
+// 'sVGNosYwuoYr+UlfAgMBAAE=',
+// '-----END PUBLIC KEY-----']
+
+module.exports = start.join('\n').trim();
